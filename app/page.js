a8f1542fcba755260d4f4fb32cbf2a70152e7b15
Naome <tuyishimenaome27@gmail.com>
2023-09-21 14:58:08 +0300

    intial commit

---
+++ app/page.js
@@ -0,0 +1,242 @@
+"use client"
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 1728px;
+  height: 4420px;
+  position: relative;
+  background-color: var(--neutral-900);
+`;
+
+const NameText = styled.div`
+  left: 139px;
+  top: 104px;
+  position: absolute;
+  color: var(--teal-500);
+  font-size: 40px;
+  font-weight: 600;
+  font-family: Inter;
+`;
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Container>
+    <NameText>name .</NameText>
+    <div className="w-[1728px] h-[4420px] relative bg-neutral-900">
+  <div className="left-[139px] top-[104px] absolute text-teal-500 text-[40px] font-semibold font-['Inter']">name .</div>
+  <div className="w-[438px] h-[33px] left-[1152px] top-[119px] absolute">
+    <div className="left-0 top-0 absolute text-teal-500 text-[27px] font-semibold font-['Inter']">Home </div>
+    <div className="left-[109px] top-0 absolute text-white text-[27px] font-semibold font-['Inter']">About </div>
+    <div className="left-[221px] top-0 absolute text-white text-[27px] font-semibold font-['Inter']">Works</div>
+    <div className="left-[333px] top-0 absolute text-white text-[27px] font-semibold font-['Inter']">Contact</div>
+  </div>
+  <div className="left-[155px] top-[370px] absolute text-white text-[50px] font-semibold font-['Inter']">About</div>
+  <div className="left-[140px] top-[604px] absolute"><span style="text-white text-[40px] font-semibold font-['Inter']">I am a </span><span style="text-teal-500 text-[40px] font-semibold font-['Inter']">Fullstack web developer</span></div>
+  <div className="w-[509px] h-[599px] left-[1081px] top-[604px] absolute bg-zinc-300" />
+  <div className="w-[796px] left-[139px] top-[698px] absolute text-white text-[40px] font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv uj  </div>
+  <div className="w-[401px] h-[211px] left-[140px] top-[992px] absolute bg-stone-900" />
+  <div className="w-[401px] h-[211px] left-[599px] top-[992px] absolute bg-stone-900" />
+  <div className="w-[1728px] h-[1434px] left-0 top-[2842px] absolute bg-neutral-950" />
+  <div className="w-[1469px] h-[741px] left-[139px] top-[3253px] absolute">
+    <div className="w-[459px] h-80 left-0 top-[421px] absolute">
+      <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+      <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+      <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+      <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+      <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+        <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+        <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+      </div>
+      <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">01</div>
+    </div>
+    <div className="w-[459px] h-80 left-[505px] top-0 absolute">
+      <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+      <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+      <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+      <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+      <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+        <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+        <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+      </div>
+      <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">01</div>
+    </div>
+    <div className="w-[459px] h-80 left-[505px] top-[421px] absolute">
+      <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+      <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+      <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+      <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+      <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+        <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+        <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+      </div>
+      <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">01</div>
+    </div>
+    <div className="w-[443px] h-80 left-[1026px] top-0 absolute">
+      <div className="w-[441px] h-[319px] left-[2px] top-[1px] absolute bg-black" />
+      <div className="w-[371px] h-[117px] left-[49px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+      <div className="left-[82px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+      <div className="w-80 h-[0px] left-[2px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+      <div className="w-[41px] h-[41px] left-0 top-[41px] absolute flex-col justify-start items-start inline-flex" />
+      <div className="left-[82px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">01</div>
+    </div>
+    <div className="w-[459px] h-80 left-[1010px] top-[421px] absolute">
+      <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+      <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+      <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+      <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+      <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+        <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+        <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+      </div>
+      <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">01</div>
+    </div>
+  </div>
+  <div className="w-[1728px] h-[1395px] left-[-15px] top-[1465px] absolute bg-neutral-900" />
+  <div className="left-[154.06px] top-[2999px] absolute text-white text-[50px] font-semibold font-['Inter']">Services</div>
+  <div className="left-[155.06px] top-[1589px] absolute text-white text-[50px] font-semibold font-['Inter']">Skill Set</div>
+  <div className="w-[459px] h-80 left-[644px] top-[3251px] absolute">
+    <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+    <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+    <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Backend  Web development</div>
+    <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+    <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+      <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+      <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+    </div>
+    <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">02</div>
+  </div>
+  <div className="w-[459px] h-80 left-[1149px] top-[3258px] absolute">
+    <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+    <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+    <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">UI/UX design</div>
+    <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+    <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+      <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+      <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+    </div>
+    <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">04</div>
+  </div>
+  <div className="w-[459px] h-80 left-[139px] top-[3674px] absolute">
+    <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+    <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+    <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+    <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+    <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+      <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+      <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+    </div>
+    <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">04</div>
+  </div>
+  <div className="w-[459px] h-80 left-[644px] top-[3673px] absolute">
+    <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+    <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+    <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+    <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+    <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+      <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+      <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+    </div>
+    <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">05</div>
+  </div>
+  <div className="w-[459px] h-80 left-[1149px] top-[3674px] absolute">
+    <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+    <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+    <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+    <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+    <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+      <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+      <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+    </div>
+    <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">06</div>
+  </div>
+  <div className="w-[459px] h-80 left-[121px] top-[3252px] absolute">
+    <div className="w-[441px] h-[319px] left-[18px] top-[1px] absolute bg-black" />
+    <div className="w-[371px] h-[117px] left-[65px] top-[140px] absolute text-white text-xl font-light font-['Inter']">Lorem ipsum dolor, sit amet consectetur adipisicing elit.uukefdsnfiuobncn uiewfhuiv if ueuifnbusujfu vhuj dhsbudf ufshhnv     </div>
+    <div className="left-[98px] top-[68px] absolute text-white text-[21px] font-semibold font-['Inter']">Frontend  Web development</div>
+    <div className="w-80 h-[0px] left-[18px] top-0 absolute origin-top-left rotate-90 border-4 border-teal-500"></div>
+    <div className="w-[72px] h-[63px] left-0 top-[30px] absolute">
+      <div className="w-[72px] h-[63px] left-0 top-0 absolute bg-teal-500" />
+      <div className="w-[41px] h-[41px] left-[16px] top-[11px] absolute" />
+    </div>
+    <div className="left-[98px] top-[40px] absolute text-teal-500 text-[17px] font-semibold font-['Inter']">01</div>
+  </div>
+  <div className="left-[315px] top-[1073px] absolute text-white text-[17px] font-semibold font-['Inter']">Phone</div>
+  <div className="left-[775px] top-[1073px] absolute text-white text-[17px] font-semibold font-['Inter']">Email</div>
+  <div className="left-[279px] top-[1102px] absolute text-white text-[17px] font-light font-['Inter']">+250781010124</div>
+  <div className="left-[731px] top-[1102px] absolute text-white text-[17px] font-light font-['Inter']">jodos@gmail.com</div>
+  <div className="w-6 h-6 left-[328px] top-[1041px] absolute" />
+  <div className="w-6 h-6 left-[784px] top-[1041px] absolute" />
+  <div className="w-[120px] h-[120px] left-[139px] top-[1808px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[935px] top-[1807px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[140px] top-[1956px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[936px] top-[1955px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[139px] top-[2104px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[935px] top-[2103px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[140px] top-[2252px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[936px] top-[2251px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[140px] top-[2400px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[120px] h-[120px] left-[936px] top-[2399px] absolute bg-teal-500 bg-opacity-20 rounded-full" />
+  <div className="w-[50px] h-[50px] left-[175px] top-[1843px] absolute" />
+  <div className="w-[50px] h-[50px] left-[175px] top-[1991px] absolute" />
+  <div className="w-[50px] h-[50px] left-[175px] top-[2139px] absolute" />
+  <div className="w-[50px] h-[50px] left-[175px] top-[2287px] absolute" />
+  <div className="w-[50px] h-[50px] left-[175px] top-[2435px] absolute" />
+  <div className="w-[50px] h-[50px] left-[970px] top-[1843px] absolute" />
+  <div className="w-[50px] h-[45.32px] left-[971px] top-[1993px] absolute" />
+  <div className="w-[490px] h-2 left-[295px] top-[1869px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[1126px] top-[1877px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[295px] top-[2016px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[1126px] top-[2024px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[295px] top-[2164px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[1126px] top-[2172px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[295px] top-[2312px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[1126px] top-[2320px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-[7.26px] left-[295px] top-[2456.74px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[4.26px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[490px] h-2 left-[1126px] top-[2464px] absolute">
+    <div className="w-[490.01px] h-[0px] left-0 top-[5px] absolute border-8 border-stone-500"></div>
+  </div>
+  <div className="w-[1728px] h-36 left-0 top-[4276px] absolute">
+    <div className="w-[1728px] h-36 left-0 top-0 absolute bg-black" />
+    <div className="w-[252.36px] h-[38.36px] left-[1356px] top-[50px] absolute">
+      <div className="w-36 h-[37.65px] left-0 top-0 absolute">
+        <div className="w-[35.23px] h-[37.58px] left-[52.36px] top-0 absolute">
+          <div className="w-[35.23px] h-[35.23px] left-0 top-0 absolute bg-neutral-950 rounded-full" />
+          <div className="w-[35.23px] h-[35.23px] left-0 top-[2.35px] absolute" />
+        </div>
+        <div className="w-[35.23px] h-[35.23px] left-[108.73px] top-[2.42px] absolute">
+          <div className="w-[35.23px] h-[35.23px] left-0 top-0 absolute bg-white rounded-full" />
+          <div className="w-[29.36px] h-[29.36px] left-[2.94px] top-[4.40px] absolute" />
+        </div>
+        <div className="w-[35.23px] h-[33.54px] left-0 top-[4px] absolute flex-col justify-start items-start inline-flex" />
+      </div>
+      <div className="w-[36.36px] h-[36.36px] left-[166px] top-[2px] absolute bg-white rounded-[20px]" />
+      <div className="w-[36.36px] h-[36.36px] left-[216px] top-[2px] absolute bg-white rounded-[20px]" />
+    </div>
+    <div className="left-[580px] top-[60px] absolute text-white text-[27px] font-light font-['Inter']">copyright @ 2023 All rights reserved</div>
+    <div className="left-[76px] top-[45px] absolute text-teal-500 text-[40px] font-semibold font-['Inter']">name .</div>
+  </div>
+</div>
+</Container>
+    </main>
+  )
+}
