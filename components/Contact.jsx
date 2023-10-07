20c73ebb31ba35f95286f607e63445d6f2f0c888
Naome <tuyishimenaome27@gmail.com>
2023-10-07 09:08:51 +0300

    end

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+    <Navbar/>
+    <p> <span className='text-green-500'>|</span> Get in touch</p>
+    <form>
+    <label>Form</label>
+    <input type='email' placeholder='email' name='email'/>
+    <label></label>
+    </form>
+    </div>
   )
 }
 
